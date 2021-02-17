f2a9c993c0c83ec80e2a2e72edffbb62e85cd2f5
Nikita Gribuska <n.gribuska@gmail.com>
2021-02-17 13:14:30 +0200

    UI added and FizzBuzz done

--- Assets/Scripts/ProgrammingTasks.cs
+++ Assets/Scripts/ProgrammingTasks.cs
@@ -15,4 +15,27 @@ public class ProgrammingTasks : MonoBehaviour
     {
         
     }
+
+    public void FizzBuzz()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            { 
+                Debug.Log("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Debug.Log("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Debug.Log("Buzz");
+            }
+            else
+            {
+                Debug.Log(i);
+            }
+        }
+    }
 }
